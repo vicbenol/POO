62857609c809746149df4bb1422013965f4ed9bd
vicbenol <victor.manuel.vmam46@gmail.com>
2020-02-27 13:30:17 -0600

    Unidad 2

---
+++ Unidad 2/MayorDeEdad/MayorDeEdad/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayorDeEdad
+{
+    public partial class lblMayor : Form
+    {
+        ClasePersona objEdad = new ClasePersona();
+        public lblMayor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            objEdad.aniosAc = int.Parse(DateTime.Today.Year.ToString());
+            objEdad.mesAc = int.Parse(DateTime.Today.Month.ToString());
+            objEdad.diaAc = int.Parse(DateTime.Today.Day.ToString());
+            objEdad.anioNac = int.Parse(dtpFecha.Value.Year.ToString());
+            objEdad.mesNac = int.Parse(dtpFecha.Value.Month.ToString());
+            objEdad.diaNac = int.Parse(dtpFecha.Value.Day.ToString());
+            objEdad.mayorEdad();
+            lblEdad.Text = objEdad.contarMayores;
+            objEdad.contarMayores = "";
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblEdad.Text = "";
+        }
+    }
+}
---
+++ Unidad 2/Temperaturas/Temperaturas/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperaturas
+{
+    public partial class Form1 : Form
+    {
+        ClaseTemperatura objtemperatura = new ClaseTemperatura();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            objtemperatura.temperatura = cmbTemperatura.Text.ToString();
+            objtemperatura.num1 =int.Parse (txtGrados.Text.ToString());
+            objtemperatura.ConversionTemperaturas();
+            lblResultadoF.Text = objtemperatura.farh.ToString();
+            lblResultadoC.Text = objtemperatura.centi.ToString();
+
+        }
+    }
+}
