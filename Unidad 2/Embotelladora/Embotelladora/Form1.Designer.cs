863b8e5d4735e647549f34e31beef7f0314f21d3
vicbenol <victor.manuel.vmam46@gmail.com>
2020-02-27 13:26:16 -0600

    Unidad 2

---
+++ Unidad 2/Embotelladora/Embotelladora/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Embotelladora
+{
+    partial class frmEmbotelladora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNivelLlenado = new System.Windows.Forms.Label();
+            this.txtNivelLlenado = new System.Windows.Forms.TextBox();
+            this.btnverificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNivelLlenado
+            // 
+            this.lblNivelLlenado.AutoSize = true;
+            this.lblNivelLlenado.Location = new System.Drawing.Point(12, 31);
+            this.lblNivelLlenado.Name = "lblNivelLlenado";
+            this.lblNivelLlenado.Size = new System.Drawing.Size(143, 13);
+            this.lblNivelLlenado.TabIndex = 0;
+            this.lblNivelLlenado.Text = "Introduce el nivel de llenado:";
+            this.lblNivelLlenado.Click += new System.EventHandler(this.lblNivelLlenado_Click);
+            // 
+            // txtNivelLlenado
+            // 
+            this.txtNivelLlenado.Location = new System.Drawing.Point(161, 28);
+            this.txtNivelLlenado.Name = "txtNivelLlenado";
+            this.txtNivelLlenado.Size = new System.Drawing.Size(100, 20);
+            this.txtNivelLlenado.TabIndex = 1;
+            this.txtNivelLlenado.TextChanged += new System.EventHandler(this.txtNivelLlenado_TextChanged);
+            // 
+            // btnverificar
+            // 
+            this.btnverificar.Location = new System.Drawing.Point(267, 26);
+            this.btnverificar.Name = "btnverificar";
+            this.btnverificar.Size = new System.Drawing.Size(75, 23);
+            this.btnverificar.TabIndex = 2;
+            this.btnverificar.Text = "Verificar";
+            this.btnverificar.UseVisualStyleBackColor = true;
+            this.btnverificar.Click += new System.EventHandler(this.btnverificar_Click);
+            // 
+            // frmEmbotelladora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 124);
+            this.Controls.Add(this.btnverificar);
+            this.Controls.Add(this.txtNivelLlenado);
+            this.Controls.Add(this.lblNivelLlenado);
+            this.Name = "frmEmbotelladora";
+            this.Text = "Verificador de mililitros ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNivelLlenado;
+        private System.Windows.Forms.TextBox txtNivelLlenado;
+        private System.Windows.Forms.Button btnverificar;
+    }
+}
+
