c90a092e1b88adcd01cd9747bb3d98e704622105
vicbenol <victor.manuel.vmam46@gmail.com>
2020-03-17 17:57:46 -0600

    Los dos trabajos estan juntos

---
+++ FigurasGeometricas/FigurasGeometricas/Cuadrado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasGeometricas
+{
+    class Cuadrado:Figura
+    {
+        public void cuadrado()
+        {
+                    Perimetro = Num1 * 4;
+                    Area = Num1 * Num1;
+        }
+    }
+}
