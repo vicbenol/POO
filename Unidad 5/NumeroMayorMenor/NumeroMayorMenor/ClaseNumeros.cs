dc7f9ea464c2ab2d0004f72f80b070ed62fbe345
vicbenol <victor.manuel.vmam46@gmail.com>
2020-05-06 13:38:27 -0500

    program

---
+++ Unidad 5/NumeroMayorMenor/NumeroMayorMenor/ClaseNumeros.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroMayorMenor
+{
+    class ClaseNumeros
+    {
+        public int Mayor { get; set; }
+        public int Menor { get; set; }
+        public int [] NumCant { get; set; }
+        public int Cant { get; set; }
+
+        public string CalcularMay(int Mayor, string maymer,int Menor)
+        {
+            int m = 0;
+            int NumeroMay = 0;
+            int r = 0;
+            int NumeroMen = 0;
+            for (int i = 0; i < NumCant.GetLength(0); i++)
+            {
+                if (NumCant[i] > Mayor)
+                {
+                    Mayor = NumCant[i];
+                    NumeroMay = m;
+                }
+                m++;
+                if (NumCant[i] < Menor)
+                {
+                    Menor = NumCant[i];
+                    NumeroMen = r;
+                }
+                r++;
+                
+            }
+            maymer = "El numero mayor es: " + Mayor+ "\nEl numero menor es: " + Menor;
+            return maymer;
+        }
+       
+        
+    }
+    
+}
---
+++ Unidad 5/NumeroMayorMenor/NumeroMayorMenor/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace NumeroMayorMenor
+{
+    public partial class Form1 : Form
+    {
+        ClaseNumeros objNumeros = new ClaseNumeros();
+        int Mayor;
+        int Menor;
+        string maymer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Darle valor a los atributos
+            objNumeros.Cant = (int)nudCanNum.Value;
+            //Implementar el nud al atributos 
+            objNumeros.Cant = (int)nudCanNum.Value
+                ;
+            objNumeros.NumCant = new int[objNumeros.Cant];
+
+            for (int i=0;i<objNumeros.NumCant.GetLength(0); i++)
+            {
+                objNumeros.NumCant[i] = Convert.ToInt32(Interaction.InputBox("Agrega el numero " + (i + 1)));
+            }
+
+
+
+
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(objNumeros.CalcularMay(Mayor, maymer, Menor),"Los numero mayor es: ", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            
+
+        }
+    }
+}
