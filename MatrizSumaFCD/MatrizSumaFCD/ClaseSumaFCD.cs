7ffeb66f5ef4ad67750ba59e6b5b7d27c765b659
vicbenol <victor.manuel.vmam46@gmail.com>
2020-05-07 19:30:37 -0500

    Unidad 5

---
+++ MatrizSumaFCD/MatrizSumaFCD/ClaseSumaFCD.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizSumaFCD
+{
+    class ClaseSumaFCD
+    {
+        public int Cant { get; set; }
+        
+        public string [,] FilCol { get; set; }
+        public int [] Fila { get; set; }
+        public int [] Colu { get; set; }
+        public int [] Diago { get; set; }
+
+       public void SumaFilas()
+        {
+            int FL=0;
+            for(int f=0;f<FilCol.GetLength(0);f++)
+            {
+                int SumaF=0;
+                int acumF=0;
+                for(int c=0;c<FilCol.GetLength(0);c++)
+                {
+                    SumaF = Convert.ToInt32(FilCol[f, c]);
+                    acumF += SumaF;
+                }
+                Fila[FL] = acumF;
+                FL++;
+            }
+        }
+        public void SumaColumnas()
+        {
+            int CL = 0;
+            for (int c = 0; c < FilCol.GetLength(0); c++)
+            {
+                int SumaC = 0;
+                int acumC = 0;
+                for (int f = 0; f < FilCol.GetLength(0); f++)
+                {
+                    SumaC = Convert.ToInt32(FilCol[f, c]);
+                    acumC += SumaC;
+                }
+                Colu[CL] = acumC;
+                CL++;
+            }
+        }
+        public string SumaDiagonal()
+        {
+            string DL;
+            int AcumD = 0;
+            for(int f=0;f<FilCol.GetLength(0);f++)
+            {
+                int SumaD = 0;
+                for(int c=0;c<FilCol.GetLength(0);c++)
+                {
+                    if(f==c)
+                    {
+                        SumaD = Convert.ToInt32(FilCol[f, c]);
+                        AcumD += SumaD;
+                    }
+                }
+            }
+            DL = Convert.ToString(AcumD);
+            return DL;    
+        }
+        public string ImprimirFilas()
+        {
+            string FL2;
+            int AcumFL = 0;
+            for(int f=0;f<FilCol.GetLength(0);f++)
+            {
+                int SumaFL;
+                for(int c=0;c<FilCol.GetLength(0);c++)
+                {
+                    SumaFL = Convert.ToInt32(FilCol[f, c]);
+                    AcumFL += SumaFL;
+                }
+            }
+            FL2 = Convert.ToString(AcumFL);
+            return FL2;
+        }
+        public string ImprimirColumnas()
+        {
+            string CL2;
+            int AcumCL = 0;
+            for (int c = 0; c < FilCol.GetLength(0); c++)
+            {
+                int SumaCL;
+                for (int f = 0; f < FilCol.GetLength(0); f++)
+                {
+                    SumaCL = Convert.ToInt32(FilCol[f, c]);
+                    AcumCL += SumaCL;
+                }
+            }
+            CL2 = Convert.ToString(AcumCL);
+            return CL2;
+        }
+
+
+    }
+}
---
+++ MatrizSumaFCD/MatrizSumaFCD/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace MatrizSumaFCD
+{
+    public partial class frmFCD : Form
+    {
+        ClaseSumaFCD objFCD = new ClaseSumaFCD();
+        string AcumF;
+        string AcumC;
+        public frmFCD()
+        {
+            InitializeComponent();
+            btnColumnas.Enabled = false;
+            btnDiagonales.Enabled = false;
+            btnFilas.Enabled = false;
+            btnTotalCol.Enabled = false;
+            btnTotalFil.Enabled = false;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            objFCD.Cant = (int)nudFilCol.Value;
+
+            objFCD.FilCol = new string[objFCD.Cant, objFCD.Cant];
+            for(int f=0; f<objFCD.FilCol.GetLength(0);f++)
+            {
+                objFCD.FilCol[f, 0] = Microsoft.VisualBasic.Interaction.InputBox("Agrega un numero " + (f + 1) + " Filas");
+               
+                for (int c = 1; c < objFCD.FilCol.GetLength(1); c++)
+                {
+                    objFCD.FilCol[f, c] = Microsoft.VisualBasic.Interaction.InputBox("Agrega otro numero " + (c + 1) + " columnas");
+                }
+            }
+            btnColumnas.Enabled = true;
+            btnDiagonales.Enabled = true;
+            btnFilas.Enabled = true;
+            btnTotalCol.Enabled = true;
+            btnTotalFil.Enabled = true;
+        }
+
+        private void btnFilas_Click(object sender, EventArgs e)
+        {
+            objFCD.Fila = new int[objFCD.Cant];
+            objFCD.SumaFilas();
+            for (int f = 0; f < objFCD.Cant; f++)
+            {
+                AcumF += objFCD.Fila[f] + " ";
+                txtSumFil.Text = AcumF;
+            }
+        }
+
+        private void btnColumnas_Click(object sender, EventArgs e)
+        {
+            objFCD.Colu = new int[objFCD.Cant];
+            objFCD.SumaColumnas();
+            for(int c=0;c<objFCD.Cant;c++)
+            {
+                AcumC += objFCD.Colu[c] + " ";
+                txtSumCol.Text = AcumC;
+            }
+        }
+
+        private void btnDiagonales_Click(object sender, EventArgs e)
+        {
+            txtSumDia.Text = objFCD.SumaDiagonal();
+        }
+
+        private void btnTotalFil_Click(object sender, EventArgs e)
+        {
+            txtTotalFil.Text = objFCD.ImprimirFilas();
+        }
+
+        private void btnTotalCol_Click(object sender, EventArgs e)
+        {
+            txtTotalCol.Text = objFCD.ImprimirColumnas();
+        }
+    }
+}
