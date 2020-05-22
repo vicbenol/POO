aac8482d38711101e1ff5fff988d5b5d666a0a6c
vicbenol <victor.manuel.vmam46@gmail.com>
2020-05-21 21:31:09 -0500

    Introduccion U6

---
+++ StreamEjemplo2/StreamEjemplo2/Clasebyte.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamEjemplo2
+{
+    class Clasebyte
+    {
+        public void Guardar()
+        {
+            using (Stream fs = new FileStream("./archivo.txt", FileMode.Open, FileAccess.Read))
+            {
+                long quantity = fs.Length;
+                Console.WriteLine($"Si jala me la jalo {quantity} veces");
+
+                for (long con = quantity; con > 0; con--)
+                {
+                    fs.Seek(-con, SeekOrigin.End);
+                    int valor = fs.ReadByte();
+                    Console.WriteLine($"Posicion{con}: {(char)valor}");
+                }
+                //Muy importante cerrarel stream
+                fs.Close();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ StreamEjemplo4/StreamEjemplo4/ClaseByte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamEjemplo4
+{
+    class ClaseByte
+    {
+        public void Guardar()
+        {
+            using (Stream fs = new FileStream("./Escribirarchivo.txt", FileMode.Create, FileAccess.Write))
+            {
+                    using(StreamWriter sw= new StreamWriter(fs))
+                {
+                    sw.WriteLine("Hola, mando este texto desde C#");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
